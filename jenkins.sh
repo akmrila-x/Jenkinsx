c5e439727378b939a923bef2f1d0eac21defe012
RILA <akmrila.x@gmail.com>
2024-08-06 11:59:28 +0530

    commit

--- jenkins.sh
+++ jenkins.sh
@@ -36,7 +36,7 @@ pipeline {
                 sh '''
                     . /home/oracle/scripts/setEnv.sh
                     sqlplus / as sysdba << EOF
-                    select username,profile,account_status from dba_users where USERNAME = upper('${USERNAME}');
+                    select profile,account_status from dba_users where USERNAME = upper('${USERNAME}');
                     exit;
                     EOF
                 '''
